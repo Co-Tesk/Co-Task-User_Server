fa04840be228acd8758c7a751885b96365ade3bb
namung08 <namung08@gmail.com>
2025-08-18 19:14:08 +0900

    :tada: feat: user-server 프로젝트 시작

---
+++ src/test/java/com/cotask/user_server/CoTaskUserServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cotask.user_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoTaskUserServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
