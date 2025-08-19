8151674dca5b15d27b7fc491efad6b468b5494ec
namung08 <namung08@gmail.com>
2025-08-19 11:45:59 +0900

    :sparkles: feat: user_server User entity 선언
    - @Setter를 클래스에서 선언 하는 것이 아닌 필드별로 선언 하여 관리합니다.
    - isDeleted 를 통해 사용자의 탈퇴 여부를 확인 합니다.

---
+++ src/main/java/com/cotask/user_server/entity/User.java
@@ -0,0 +1,57 @@
+package com.cotask.user_server.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true, length = 100)
+	private String email;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = false, length = 30)
+	@Setter
+	private String nickname;
+
+	@Setter
+	private Boolean isVerify = false;
+
+	@CreationTimestamp
+	@Column(nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+
+	@UpdateTimestamp
+	@Column(nullable = false)
+	private LocalDateTime updatedAt;
+
+	@Setter
+	private Boolean isDeleted = false;
+
+	@Setter
+	private LocalDateTime deletedAt = null;
+}
