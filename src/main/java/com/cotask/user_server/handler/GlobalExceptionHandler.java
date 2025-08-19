b10c0b964756d776a9913b41031357163b9175b2
namung08 <namung08@gmail.com>
2025-08-19 14:47:16 +0900

    :fire: remove: jwt의 만료 검증을 API-Gateway 에 위임할 예정이므로 삭제

--- src/main/java/com/cotask/user_server/handler/GlobalExceptionHandler.java
+++ src/main/java/com/cotask/user_server/handler/GlobalExceptionHandler.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import com.cotask.user_server.dto.response.CommonResponse;
 import com.cotask.user_server.dto.response.ExceptionResponse;
 
-import io.jsonwebtoken.ExpiredJwtException;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -38,22 +37,6 @@ public class GlobalExceptionHandler {
 					"VALIDATION_ERROR",
 					"입력값이 유효하지 않습니다.",
 					errors // 필드별 메시지 포함
-				)
-			));
-	}
-
-	// JWT 만료 예외 헨들러
-	@ExceptionHandler(ExpiredJwtException.class)
-	public ResponseEntity<?> handleExpiredJwtException(ExpiredJwtException e) {
-		log.warn("JWT 토큰 만료: {}", e.getMessage());
-
-		return ResponseEntity
-			.status(HttpStatus.UNAUTHORIZED)
-			.body(CommonResponse.fail(
-				ExceptionResponse.of(
-					HttpStatus.UNAUTHORIZED,
-					"JWT_EXPIRED",
-					"인증 토큰이 만료되었습니다. 다시 로그인해주세요."
 				)
 			));
 	}
