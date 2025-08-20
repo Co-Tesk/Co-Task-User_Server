1f22a29c01595e347373f5a072d74d8f68ea0953
namung08 <namung08@gmail.com>
2025-08-20 14:45:03 +0900

    :sparkles: feat: 회원가입 비즈니스 로직 메서드 선언 및 구현
     - 회원가입 정보 확인
     - 사용자 정보 저장
     - 인증 코드 저장
     - 추후 kafka를 통해 이메일을 발송할 수 있도록 해야함
    
    Signed-off-by: namung08 <namung08@gmail.com>

--- src/main/java/com/cotask/user_server/service/UserServerService.java
+++ src/main/java/com/cotask/user_server/service/UserServerService.java
@@ -1,4 +1,15 @@
 package com.cotask.user_server.service;
 
+import com.cotask.user_server.dto.request.Register;
+import com.cotask.user_server.infrastructure.exception.CoTaskException;
+
 public interface UserServerService {
+	/**
+	 * 사용자의 회원가입을 처리하는 메서드
+	 * 회원가입 시 이메일 중복 확인, 비밀번호 일치 여부 확인 후
+	 * 사용자 정보를 저장하고 이메일 인증 토큰을 생성하여 저장한다.
+	 * @throws CoTaskException 이메일 중복 또는 비밀번호 불일치 시 예외 발생
+	 * @param register 회원가입에 필요한 정보를 담고 있는 DTO
+	 */
+	void register(Register register);
 }
--- src/main/java/com/cotask/user_server/service/UserServerServiceImpl.java
+++ src/main/java/com/cotask/user_server/service/UserServerServiceImpl.java
@@ -1,6 +1,17 @@
 package com.cotask.user_server.service;
 
+import java.time.Instant;
+import java.util.UUID;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.cotask.user_server.dto.request.Register;
+import com.cotask.user_server.entity.User;
+import com.cotask.user_server.entity.Verify;
+import com.cotask.user_server.entity.VerifyType;
+import com.cotask.user_server.infrastructure.exception.CoTaskException;
+import com.cotask.user_server.infrastructure.exception.CoTaskExceptionCode;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,6 +21,41 @@ public class UserServerServiceImpl implements UserServerService {
 	private final SocialAccountService socialAccountService;
 	private final VerifyService verifyService;
 	private final UserService userService;
-	private final TokenService tokenService;
+	private final PasswordEncoder passwordEncoder;
 
+	@Override
+	public void register(Register register) {
+		registerVerify(register);
+		User savedUser = userService.save(User.builder()
+			.email(register.email())
+			.password(passwordEncoder.encode(register.getPassword()))
+			.nickname(register.nickname())
+			.isVerify(false)
+			.build());
+		// 이메일 전송의 경우 kafka 를 사용해 비동기로 처리하도록 해야함.
+		// 이메일 인증 토큰 생성 및 저장
+		Verify savedVerify = verifyService.save(Verify.builder()
+			.user(savedUser)
+			.type(VerifyType.EMAIL)
+			.code(UUID.randomUUID())
+			.expiresAt(Instant.now().plusSeconds(3600)) // 1시간 후 만료
+			.isUsed(false)
+			.build());
+		// TODO: kafka 를 사용한 이메일 전송 로직 추가 필요
+	}
+
+	private void registerVerify(Register register) {
+		// 이메일 중복 확인
+		if (userService.existsByEmail(register.email())) {
+			throw new CoTaskException(CoTaskExceptionCode.DUPLICATE_EMAIL);
+		}
+		// 비밀번호 동일 3차 확인(1차: front, 2차: dto, 3차: registerVerify)
+		passwordConfirmMatch(register.password(), register.confirmPassword());
+	}
+
+	private void passwordConfirmMatch(String password, String passwordConfirm) {
+		if (!password.equals(passwordConfirm)) {
+			throw new CoTaskException(CoTaskExceptionCode.PASSWORD_CONFIRM_NOT_MATCH);
+		}
+	}
 }
