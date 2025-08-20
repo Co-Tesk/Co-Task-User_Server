6bd80756f4805e3f2d95fac70ca53b6bcd0a2c2e
namung08 <namung08@gmail.com>
2025-08-20 14:30:59 +0900

    :sparkles: feat: 회원가입 엔드포인트 공개 주소 설정
    Signed-off-by: namung08 <namung08@gmail.com>

--- src/main/java/com/cotask/user_server/infrastructure/utils/Constants.java
+++ src/main/java/com/cotask/user_server/infrastructure/utils/Constants.java
@@ -3,6 +3,7 @@ package com.cotask.user_server.infrastructure.utils;
 public class Constants {
 	public static final String[] PUBLIC_PATH = {
 		"/actuator/**",
+		"/users/register"
 	};
 	// token constants
 	public static final String ACCESS_TOKEN_HEADER = "X-ACCESS-TOKEN";
