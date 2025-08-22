dc06c5287a56396cd74e8a739117f15b8b42de4b
namung08 <namung08@gmail.com>
2025-08-22 20:06:14 +0900

    :sparkles: feat: User 객체와 is_used 필드가 false 인 인증 리스트 조회 메서드 선언
    Signed-off-by: namung08 <namung08@gmail.com>

--- src/main/java/com/cotask/user_server/repository/VerifyRepository.java
+++ src/main/java/com/cotask/user_server/repository/VerifyRepository.java
@@ -1,8 +1,12 @@
 package com.cotask.user_server.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.cotask.user_server.entity.User;
 import com.cotask.user_server.entity.Verify;
 
 public interface VerifyRepository extends JpaRepository<Verify, Long> {
+	List<Verify> findByUserAndIsUsedFalse(User user);
 }
