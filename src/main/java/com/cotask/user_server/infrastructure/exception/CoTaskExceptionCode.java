895e206a82034729971829ae7166a2183cce8fe1
namung08 <namung08@gmail.com>
2025-08-22 20:11:21 +0900

    :sparkles: feat: 사용자 인증 시 발생 하는 에러 목록 추가
    Signed-off-by: namung08 <namung08@gmail.com>

--- src/main/java/com/cotask/user_server/infrastructure/exception/CoTaskExceptionCode.java
+++ src/main/java/com/cotask/user_server/infrastructure/exception/CoTaskExceptionCode.java
@@ -23,7 +23,30 @@ public enum CoTaskExceptionCode {
 		"CO_TASK_002",
 		"비밀번호가 일치하지 않습니다."
 	),
-	;
+	NOT_FOUND_USER(
+		HttpStatus.NOT_FOUND,
+		"CO_TASK_003",
+		"사용자를 찾을 수 없습니다."
+	),
+	NOT_FOUND_VERIFICATION(
+		HttpStatus.NOT_FOUND,
+		"CO_TASK_004",
+		"사용자 인증 정보를 찾을 수 없습니다."
+	),
+	NOT_MATCH_VERIFICATION(
+		HttpStatus.BAD_REQUEST,
+		"CO_TASK_005",
+		"인증 정보가 일치하지 않습니다."), EXPIRED_VERIFICATION(
+		HttpStatus.BAD_REQUEST,
+		"CO_TASK_006",
+		"인증 정보가 만료되었습니다."
+	),
+	ALREADY_USED_VERIFICATION(
+		HttpStatus.BAD_REQUEST,
+		"CO_TASK_007",
+		"이미 사용된 인증 정보입니다."
+	);
+
 
 	private final HttpStatus status;
 	private final String code;
