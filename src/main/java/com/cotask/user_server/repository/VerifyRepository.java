72f55dfb186e1935722ad0deaf279576d1ce6cdf
namung08 <namung08@gmail.com>
2025-08-26 18:13:44 +0900

    :zap: perf: 낙관적 잠금 기능을 사용한 동시 검증 이중 사용 방지
    Signed-off-by: namung08 <namung08@gmail.com>

--- src/main/java/com/cotask/user_server/repository/VerifyRepository.java
+++ src/main/java/com/cotask/user_server/repository/VerifyRepository.java
@@ -1,11 +1,17 @@
 package com.cotask.user_server.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 import com.cotask.user_server.entity.User;
 import com.cotask.user_server.entity.Verify;
+import com.cotask.user_server.entity.VerifyType;
+
+import jakarta.persistence.LockModeType;
 
 public interface VerifyRepository extends JpaRepository<Verify, Long> {
 	/**
@@ -15,4 +21,8 @@ public interface VerifyRepository extends JpaRepository<Verify, Long> {
  * @return 해당 사용자에 대해 아직 사용되지 않은 Verify 엔티티들의 리스트(없으면 빈 리스트)
  */
 List<Verify> findByUserAndIsUsedFalse(User user);
+
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	@Query("select v from Verify v where v.user = :user and v.type = :type and v.code = :code")
+	Optional<Verify> findTopByUserAndTypeAndCodeForUpdate(User user, VerifyType type, String code);
 }
--- src/main/java/com/cotask/user_server/service/VerifyServiceImpl.java
+++ src/main/java/com/cotask/user_server/service/VerifyServiceImpl.java
@@ -1,7 +1,6 @@
 package com.cotask.user_server.service;
 
 import java.time.Instant;
-import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -53,23 +52,10 @@ public class VerifyServiceImpl implements VerifyService {
 	@Override
 	@Transactional
 	public void validate(Verification verification, User user) {
-		// User와 isUsed가 false인 Verify 엔티티를 조회
-		List<Verify> verifyList = repository.findByUserAndIsUsedFalse(user);
-		// 사용 전인 인증 정보가 없으면 예외 발생
-		if (verifyList.isEmpty()) {
-			throw new CoTaskException(CoTaskExceptionCode.NOT_FOUND_VERIFICATION);
-		}
-		Verify checkedVerify = verifyList.stream()
-			// verification.type()이 Verify의 type과 일치하는지 확인
-			.filter(verify -> verify.getType().equals(verification.type()))
-			// verification.code()가 Verify의 code와 일치하는지 확인
-			.filter(verify -> verify.getCode().equals(verification.verificationCode()))
-			// 만약 일치 할 경우 1개만 존재해야 하므로 findFirst()로 첫 번째 요소를 가져옴
-			.findFirst()
-			// 일치하는 인증 정보가 없으면 예외 발생
-			.orElseThrow(
-				() -> new CoTaskException(CoTaskExceptionCode.NOT_FOUND_VERIFICATION)
-			);
+
+		Verify checkedVerify = repository
+			.findTopByUserAndTypeAndCodeForUpdate(user, verification.type(), verification.verificationCode())
+			.orElseThrow(() -> new CoTaskException(CoTaskExceptionCode.NOT_FOUND_VERIFICATION));
 		// 인증 정보 사용 여부 확인
 		if (checkedVerify.getIsUsed()) {
 			throw new CoTaskException(CoTaskExceptionCode.ALREADY_USED_VERIFICATION);
