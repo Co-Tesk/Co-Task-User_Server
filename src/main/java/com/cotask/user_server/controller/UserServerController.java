ef0c2c196da9fc1b515ce0f5cf3d3f2da10be1f1
namung08 <namung08@gmail.com>
2025-08-27 18:57:32 +0900

    :memo: docs: 에러 응답 설정을 바뀐 코드에 따른 수정
    Signed-off-by: namung08 <namung08@gmail.com>

--- src/main/java/com/cotask/user_server/controller/UserServerController.java
+++ src/main/java/com/cotask/user_server/controller/UserServerController.java
@@ -1,14 +1,14 @@
 package com.cotask.user_server.controller;
 
+import static com.cotask.user_server.infrastructure.exception.CoTaskExceptionCode.*;
+
 import org.springframework.http.ResponseEntity;
 
+import com.cotask.user_server.annotation.swagger.ApiErrorCodeExamples;
 import com.cotask.user_server.dto.request.Register;
 import com.cotask.user_server.dto.request.Verification;
-import com.cotask.user_server.dto.response.CommonResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 public interface UserServerController {
@@ -21,87 +21,14 @@ public interface UserServerController {
 	@Operation(
 		summary = "사용자 회원가입",
 		description = "사용자가 회원가입을 요청할 때 호출되는 메서드입니다. <br>" +
-			"회원가입 시 필요한 정보를 담고 있는 DTO를 받아 처리합니다.",
-		responses = {
-			@ApiResponse(
-				responseCode = "201",
-				description = "회원가입 성공",
-				content = @Content(
-					schema = @Schema(
-						implementation = void.class,
-						description = "회원가입이 성공적으로 처리되면 빈 응답을 반환합니다。"
-					)
-				)
-			),
-			@ApiResponse(
-				responseCode = "400",
-				description = "Validation 실패",
-				content = @Content(
-					mediaType = "application/json",
-					schema = @Schema(
-						implementation = CommonResponse.class,
-						description = "유효성 검사 실패 시 반환되는 에러 메시지",
-						example = """
-								{
-									"success": false,
-									"error": {
-										"status": 400,
-										"code": "VALIDATION_ERROR",
-										"message": "입력값이 유효하지 않습니다.",
-										"errors": {
-											"email": "이메일은 100자 이하이어야 합니다.",
-											"nickname": "닉네임은 필수 입력 항목입니다."
-										}
-									}
-								}
-							"""
-					)
-				)
-			),
-			@ApiResponse(
-				responseCode = "409",
-				description = "이메일 중복",
-				content = @Content(
-					mediaType = "application/json",
-					schema = @Schema(
-						implementation = CommonResponse.class,
-						description = "유효성 검사 실패 시 반환되는 에러 메시지",
-						example = """
-								{
-									"success": false,
-									"error": {
-										"status": 409,
-										"code": "CO_TASK_001",
-										"message": "이미 사용중인 이메일입니다."
-									}
-								}
-							"""
-					)
-				)
-			),
-			@ApiResponse(
-				responseCode = "500",
-				description = "서버 오류",
-				content = @Content(
-					mediaType = "application/json",
-					schema = @Schema(
-						implementation = CommonResponse.class,
-						description = "서버 오류 발생 시 반환되는 에러 메시지",
-						example = """
-								{
-									"success": false,
-									"error": {
-										"status": 500,
-										"code": "INTERNAL_SERVER_ERROR",
-										"message": "An internal server error occurred."
-									}
-								}
-							"""
-					)
-				)
-			)
-		}
+			"회원가입 시 필요한 정보를 담고 있는 DTO를 받아 처리합니다."
 	)
+	@ApiErrorCodeExamples({
+		DUPLICATE_EMAIL,
+		PASSWORD_CONFIRM_NOT_MATCH,
+		INTERNAL_SERVER_ERROR
+	})
+	@ApiResponse(responseCode = "201", description = "Created - 회원가입 성공")
 	ResponseEntity<?> register(Register register);
 
 	/**
@@ -116,148 +43,15 @@ public interface UserServerController {
 	@Operation(
 		summary = "사용자 인증",
 		description = "사용자 검증 요청을 처리하는 API입니다.<br>" +
-			"Verification DTO를 받아 검증을 수행합니다.",
-		responses = {
-			@ApiResponse(
-				responseCode = "204",
-				description = "검증 성공",
-				content = @Content(
-					schema = @Schema(
-						implementation = void.class,
-						description = "검증이 성공적으로 처리되면 빈 응답을 반환합니다。"
-					)
-				)
-			),
-			@ApiResponse(
-				responseCode = "400",
-				description = "Validation 실패",
-				content = @Content(
-					mediaType = "application/json",
-					schema = @Schema(
-						implementation = CommonResponse.class,
-						description = "유효성 검사 실패 시 반환되는 에러 메시지",
-						example = """
-								{
-									"success": false,
-									"error": {
-										"status": 400,
-										"code": "VALIDATION_ERROR",
-										"message": "입력값이 유효하지 않습니다.",
-										"errors": {
-											"email": "이메일은 100자 이하이어야 합니다."
-										}
-									}
-								}
-							"""
-					)
-				)
-			),
-			@ApiResponse(
-				responseCode = "400",
-				description = "인증 정보를 이미 사용한 경우",
-				content = @Content(
-					mediaType = "application/json",
-					schema = @Schema(
-						implementation = CommonResponse.class,
-						description = "인증 정보를 이미 사용한 경우 반환되는 에러 메시지",
-						example = """
-								{
-									"success": false,
-									"error": {
-										"status": 400,
-										"code": "CO_TASK_007",
-										"message": "이미 사용된 인증 정보입니다."
-									}
-								}
-							"""
-					)
-				)
-			),
-			@ApiResponse(
-				responseCode = "400",
-				description = "만료된 인증 정보",
-				content = @Content(
-					mediaType = "application/json",
-					schema = @Schema(
-						implementation = CommonResponse.class,
-						description = "만료된 인증 정보 반환되는 에러 메시지",
-						example = """
-								{
-									"success": false,
-									"error": {
-										"status": 400,
-										"code": "CO_TASK_006",
-										"message": "인증 정보가 만료되었습니다."
-									}
-								}
-							"""
-					)
-				)
-			),
-			@ApiResponse(
-				responseCode = "404",
-				description = "사용자 조회 실패",
-				content = @Content(
-					mediaType = "application/json",
-					schema = @Schema(
-						implementation = CommonResponse.class,
-						description = "사용자 조회 실패 시 반환되는 에러 메시지",
-						example = """
-								{
-									"success": false,
-									"error": {
-										"status": 404,
-										"code": "CO_TASK_003",
-										"message": "사용자를 찾을 수 없습니다."
-									}
-								}
-							"""
-					)
-				)
-			),
-			@ApiResponse(
-				responseCode = "404",
-				description = "사용자 인증 정보 조회 실패",
-				content = @Content(
-					mediaType = "application/json",
-					schema = @Schema(
-						implementation = CommonResponse.class,
-						description = "사용자 인증 정보 조회 실패 시 반환되는 에러 메시지",
-						example = """
-								{
-									"success": false,
-									"error": {
-										"status": 404,
-										"code": "CO_TASK_004",
-										"message": "사용자 인증 정보를 찾을 수 없습니다."
-									}
-								}
-							"""
-					)
-				)
-			),
-			@ApiResponse(
-				responseCode = "500",
-				description = "서버 오류",
-				content = @Content(
-					mediaType = "application/json",
-					schema = @Schema(
-						implementation = CommonResponse.class,
-						description = "서버 오류 발생 시 반환되는 에러 메시지",
-						example = """
-								{
-									"success": false,
-									"error": {
-										"status": 500,
-										"code": "INTERNAL_SERVER_ERROR",
-										"message": "An internal server error occurred."
-									}
-								}
-							"""
-					)
-				)
-			)
-		}
+			"Verification DTO를 받아 검증을 수행합니다."
 	)
+	@ApiErrorCodeExamples({
+		NOT_FOUND_USER,
+		NOT_FOUND_VERIFICATION,
+		ALREADY_USED_VERIFICATION,
+		EXPIRED_VERIFICATION,
+		INTERNAL_SERVER_ERROR
+	})
+	@ApiResponse(responseCode = "204", description = "No Content - 인증 성공")
 	ResponseEntity<?> verification(Verification verification);
 }
