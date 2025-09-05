6c0420bdb117db3ca0d92a1677c83228cc4d0b31
namung08 <namung08@gmail.com>
2025-09-05 10:31:48 +0900

    :sparkles: feat: 로그인 경로 공개 및 토큰 만료 시간 설정
    Signed-off-by: namung08 <namung08@gmail.com>

--- src/main/java/com/cotask/user_server/infrastructure/utils/Constants.java
+++ src/main/java/com/cotask/user_server/infrastructure/utils/Constants.java
@@ -1,10 +1,13 @@
 package com.cotask.user_server.infrastructure.utils;
 
+import java.time.Duration;
+
 public class Constants {
 	public static final String[] PUBLIC_PATH = {
 		"/actuator/**",
 		"/users/auth/register",
 		"/users/auth/verification",
+		"/users/auth/login"
 	};
 
 	public static final String[] SWAGGER_PATH = {
@@ -15,6 +18,9 @@ public class Constants {
 	// token constants
 	public static final String ACCESS_TOKEN_HEADER = "X-ACCESS-TOKEN";
 	public static final String REFRESH_TOKEN_COOKIE = "X-REFRESH-TOKEN";
+
+	public static final Long ACCESS_EXPIRE_MS = Duration.ofMinutes(30L).toMillis();
+	public static final Long REFRESH_EXPIRE_MS = Duration.ofDays(7L).toMillis();
 	private Constants() {
 	}
 
